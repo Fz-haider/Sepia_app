45a60415dafb8d52f57b2708e06cfe5216c18e73
Fz-haider <haiderahmad393@gmail.com>
2022-10-24 11:45:28 +0300

    Initial commit

---
+++ lib/parent_Pages/parent_id_submit.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:sepia_app/custom_widget.dart';
+
+class parent_id_submit extends StatelessWidget {
+  const parent_id_submit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Parent"),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                width: 250,
+                child: customField(
+                  "Enter Your ID",
+                  () {},
+                  maxLength: 6,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            customIconButton(
+              Icons.arrow_circle_right_outlined,
+              "Submit",
+              () {
+                Navigator.of(context).pushReplacementNamed("parent_home_page");
+              },
+              paddingVertical: 15,
+              paddingHorizontal: 15,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
